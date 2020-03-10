1fe8b2131df144916930fca5b3c73c608a3fce60
Rachel Steinhorst <steinhor@msu.edu>
2020-03-10 10:58:28 -0400

    add for loop for copying files

--- figs/paper/scripts/collect_data.sh
+++ figs/paper/scripts/collect_data.sh
@@ -27,7 +27,9 @@ cd ../../..
 cd figs/paper/data
 
 for i in 0 1 2 3 4 5 6
+do
   cp roots${i}_T140.dat roots${i}_Omega100.dat
   cp roots${i}_T140.dat roots${i}_rhoB0.05.dat
   cp roots${i}_T140.dat roots${i}_eta0.6.dat
   cp roots${i}_T140.dat roots${i}_Acc0.4.dat
+done
