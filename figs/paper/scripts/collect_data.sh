b0d35860b0a0356eeee43ed21b69a32d0391135f
Rachel Steinhorst <steinhor@msu.edu>
2020-03-10 09:58:46 -0400

    add script to run all events needed for paper figs

---
+++ figs/paper/scripts/collect_data.sh
@@ -0,0 +1,25 @@
+
+cd ../../..
+
+./bw figs/paper/pars/vsT/T110.dat > figs/paper/crap/crap1.dat &
+./bw figs/paper/pars/vsT/T125.dat > figs/paper/crap/crap2.dat &
+./bw figs/paper/pars/vsT/T140.dat > figs/paper/crap/crap3.dat &
+./bw figs/paper/pars/vsT/T155.dat > figs/paper/crap/crap4.dat &
+
+./bw figs/paper/pars/vsrhoB/rhoB0.1.dat > figs/paper/crap/crap5.dat &
+./bw figs/paper/pars/vsrhoB/rhoB0.2.dat > figs/paper/crap/crap6.dat &
+./bw figs/paper/pars/vsrhoB/rhoB0.15.dat > figs/paper/crap/crap8.dat &
+./bw figs/paper/pars/vsrhoB/rhoB0.dat > figs/paper/crap/crap9.dat &
+
+./bw figs/paper/pars/vsOmega/Omega50.dat > figs/paper/crap/crap10.dat &
+./bw figs/paper/pars/vsOmega/Omega250.dat > figs/paper/crap/crap12.dat &
+
+./bw figs/paper/pars/vsEta/eta0.2.dat > figs/paper/crap/crap13.dat &
+./bw figs/paper/pars/vsEta/eta1.0.dat > figs/paper/crap/crap13.dat &
+
+./bw figs/paper/pars/vsAcc/Acc0.2.dat > figs/paper/crap/crap14.dat &
+./bw figs/paper/pars/vsAcc/Acc0.5.dat > figs/paper/crap/crap15.dat &
+./bw figs/paper/pars/vsAcc/Acc0.6.dat > figs/paper/crap/crap16.dat &
+./bw figs/paper/pars/vsAcc/Acc0.8.dat > figs/paper/crap/crap17.dat &
+./bw figs/paper/pars/vsAcc/Acc0.dat > figs/paper/crap/crap18.dat &
+./bw figs/paper/pars/vsAcc/Acc1.dat > figs/paper/crap/crap19.dat &
