a62dd5789b521c82e7126c8802b6cfe45b7a49ef
Rachel Steinhorst <steinhor@msu.edu>
2020-03-28 20:52:36 -0500

    add rachelrun directory

--- figs/paper/scripts/copy_files.sh
+++
@@ -1,9 +0,0 @@
-cd ../altdata
-
-for i in 0 1 2 3 4 5 6
-do
-  cp roots${i}_T140.dat roots${i}_Omega100.dat
-  cp roots${i}_T140.dat roots${i}_rhoB0.05.dat
-  cp roots${i}_T140.dat roots${i}_eta0.6.dat
-  cp roots${i}_T140.dat roots${i}_Acc0.4.dat
-done
---
+++ rachelrun/scripts/collect_data.sh
@@ -0,0 +1,28 @@
+
+cd ..
+make bw
+
+./bw pars/vsT/T110.dat > crap/crap1.dat &
+./bw pars/vsT/T125.dat > crap/crap2.dat &
+./bw pars/vsT/T140.dat > crap/crap3.dat &
+./bw pars/vsT/T155.dat > crap/crap4.dat &
+
+./bw pars/vsrhoB/rhoB0.1.dat > crap/crap5.dat &
+./bw pars/vsrhoB/rhoB0.2.dat > crap/crap6.dat &
+./bw pars/vsrhoB/rhoB0.15.dat > crap/crap8.dat &
+./bw pars/vsrhoB/rhoB0.dat > crap/crap9.dat &
+
+./bw pars/vsOmega/Omega50.dat > crap/crap10.dat &
+./bw pars/vsOmega/Omega250.dat > crap/crap7.dat &
+
+./bw pars/vsEta/eta0.2.dat > crap/crap11.dat &
+./bw pars/vsEta/eta1.0.dat > crap/crap12.dat &
+
+./bw pars/vsAcc/Acc0.01.dat > crap/crap13.dat &
+./bw pars/vsAcc/Acc0.2.dat > crap/crap14.dat &
+./bw pars/vsAcc/Acc0.5.dat > crap/crap15.dat &
+./bw pars/vsAcc/Acc0.6.dat > crap/crap16.dat &
+./bw pars/vsAcc/Acc0.8.dat > crap/crap17.dat &
+./bw pars/vsAcc/Acc0.dat > crap/crap18.dat &
+./bw pars/vsAcc/Acc1.dat > crap/crap19.dat &
+./bw pars/vsAcc/Acc0.99.dat > crap/crap27.dat &
