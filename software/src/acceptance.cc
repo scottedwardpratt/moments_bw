950ed5410c5c93f89fc2bf6660bc82dc0e63c7e3
scottedwardpratt <prattsc@msu.edu>
2020-07-29 09:14:15 -0400

    found newer STAR data for protons

--- software/src/acceptance.cc
+++ software/src/acceptance.cc
@@ -4,11 +4,14 @@ using namespace std;
 
 void Cacceptance::Acceptance(Cpart *part,bool &acceptQ,bool &acceptP,bool &acceptK,bool &acceptPi,bool &acceptB){
 	bool STAR = true;
+	bool EFFICIENCY_CORRECTED=true;
 	double r=randy->ran();
 	acceptQ=acceptP=acceptK=acceptPi=true;
+	double efficiency;
 
 	if (STAR) {
 		double y,eta,pt,pmag,ptmin=200.0,ptmax=2000.0;
+		
 		CResInfo *resinfo=part->resinfo;
 		pt=sqrt(part->p[1]*part->p[1]+part->p[2]*part->p[2]);
 		pmag=sqrt(pt*pt+part->p[3]*part->p[3]);
@@ -24,9 +27,23 @@ void Cacceptance::Acceptance(Cpart *part,bool &acceptQ,bool &acceptP,bool &accep
 			ptmax=2000.0;
 		}
 
-		if(resinfo->charge==0 || fabs(eta)>0.5 || pt<150.0 || r>0.8){
+		// For net charge
+		ptmin=200.0;
+		ptmax=2000.0;
+		efficiency=0.715;
+		if(EFFICIENCY_CORRECTED)
+			efficiency=1.0;
+		if(resinfo->charge==0 || fabs(eta)>0.5 || pt<ptmin || pt>ptmax || r>efficiency){
 			acceptQ=acceptP=acceptK=acceptPi=false;
 		}
+		
+		// For specific species
+		if(abs(pid)==2212){
+			ptmin=400.0;
+			ptmax=2000.0;
+			if(fabs(eta)>0.5 || p)
+		}
+		
 		else{
 			acceptP=acceptK=acceptPi=true;
 			if(pt>ptmax || pt<ptmin || fabs(eta)>0.5 || r>0.6){
